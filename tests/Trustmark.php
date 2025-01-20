7611c675248a1aef91f7550c50c7f6fbdd913982
bomsiwor <bomsiwor@gmail.com>
2025-01-20 10:26:42 +0700

    add trustmark instance startup test

---
+++ tests/Trustmark.php
@@ -0,0 +1,36 @@
+<?php
+
+use Bomsiwor\Trustmark\Contracts\ClientContract;
+use Bomsiwor\Trustmark\Core\Clients\VClaimClient;
+use Bomsiwor\Trustmark\Core\Trustmark;
+use Bomsiwor\Trustmark\Exceptions\TrustmarkException;
+
+// Scoped variable on this test
+$consId = '123abc';
+$secretKey = 'secretAlways';
+$userKey = 'userkey';
+
+it('create a client based on service id', function (string $serviceId, string $serviceClass) use ($consId, $userKey, $secretKey) {
+
+    $trustmarkClient = Trustmark::client($consId, $secretKey, $userKey, $serviceId, 'production');
+
+    expect($trustmarkClient)->toBeInstanceOf(ClientContract::class)->toBeInstanceOf($serviceClass);
+})
+    ->with([
+        ['vclaim', VClaimClient::class],
+    ]);
+
+it('throw error on invalid service id', function () use ($consId, $userKey, $secretKey) {
+
+    expect(fn () => Trustmark::client($consId, $secretKey, $userKey, 'random', 'production'))
+        ->toThrow(TrustmarkException::class);
+});
+
+it('create a client via factory', function () {
+    $trustmarkClient = Trustmark::factory(VClaimClient::class)
+        ->withBaseUrl('http://trustmark.test')
+        ->withTimestamp(time())
+        ->make();
+
+    expect($trustmarkClient)->toBeInstanceOf(ClientContract::class)->toBeInstanceOf(VClaimClient::class);
+});
