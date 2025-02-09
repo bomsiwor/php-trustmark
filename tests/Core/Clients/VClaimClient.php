4ce4a8dd6dfcc140622540bac3646e74901276d0
bomsiwor <bomsiwor@gmail.com>
2025-02-09 13:57:12 +0700

    remove var dump

--- tests/Core/Clients/VClaimClient.php
+++ tests/Core/Clients/VClaimClient.php
@@ -5,19 +5,18 @@ use Bomsiwor\Trustmark\Resources\VClaim\Referensi;
 use Bomsiwor\Trustmark\Resources\VClaim\SEP;
 
 beforeEach(function () {
-    // Initiate client
-    $consId = '123abc';
-    $secretKey = 'secretAlways';
-    $userKey = 'userkey';
+  // Initiate client
+  $consId = '123abc';
+  $secretKey = 'secretAlways';
+  $userKey = 'userkey';
 
-    $this->client = Trustmark::client($consId, $secretKey, $userKey, 'vclaim');
-    var_dump($this->client);
+  $this->client = Trustmark::client($consId, $secretKey, $userKey, 'vclaim');
 });
 
 it('has referensi subservice', function () {
-    expect($this->client->referensi())->toBeInstanceOf(Referensi::class);
+  expect($this->client->referensi())->toBeInstanceOf(Referensi::class);
 });
 
 it('has sep subservice', function () {
-    expect($this->client->sep())->toBeInstanceOf(SEP::class);
+  expect($this->client->sep())->toBeInstanceOf(SEP::class);
 });
